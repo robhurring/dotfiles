8a6d8aed3c9562a3aecb77ab43574a851b3cff94
rob <rob@hades.home>
2011-04-27 20:25:16 -0400

    using install.rb to symlink the repo rather than using tarballs extracted in $HOME -- should make management a bit easier

---
+++ install.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+# from http://errtheblog.com/posts/89-huba-huba
+
+home = ENV['HOME']
+ignore = %{bin install.rb .mybashrc}
+force = ARGV[0] == '-f'
+dotfiles_dir = File.expand_path File.dirname(__FILE__)
+
+Dir.chdir dotfiles_dir do
+  (Dir['*']+Dir['bin/*']).each do |file|
+    next if ignore.include?(file)
+    target_name = file =~ /^bin/ ? file : ".#{file}"
+    target = File.join(home, target_name)
+    if force || !File.exist?(target)
+      system %[ln -vsf #{File.join(dotfiles_dir, file)} #{target}]
+    end
+  end
+end
+puts <<-EOF
+
+Make sure to change your git details in ~/.mybashrc
+ 
+  GIT_COMMITTER_NAME
+  GIT_COMMITTER_EMAIL
+
+See .mybashrc for example usage.
+
+EOF
