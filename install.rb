ca656203d21a0e989e47fc3a5d5be3bb069e6e4c
robhurring <robhurring@gmail.com>
2011-04-27 21:03:49 -0400

    fixed install script

--- install.rb
+++ install.rb
@@ -1,21 +1,24 @@
 #!/usr/bin/env ruby
 # from http://errtheblog.com/posts/89-huba-huba
 
-home = ENV['HOME']
-ignore = %{bin install.rb .mybashrc}
-force = ARGV[0] == '-f'
-dotfiles_dir = File.expand_path File.dirname(__FILE__)
+$force = ARGV[0] == '-f'
+$home = ENV['HOME']
+$here = File.expand_path File.dirname(__FILE__)
+$ignore = %w{.mybashrc.example .git}
 
-Dir.chdir dotfiles_dir do
-  (Dir['*']+Dir['bin/*']).each do |file|
-    next if ignore.include?(file)
-    target_name = file =~ /^bin/ ? file : ".#{file}"
-    target = File.join(home, target_name)
-    if force || !File.exist?(target)
-      system %[ln -vsf #{File.join(dotfiles_dir, file)} #{target}]
+# link dotfiles
+Dir.chdir $here do
+  (Dir['.*'] + Dir['bin/*']).each do |file|
+    next if file =~ /^\.{1,2}$/ || $ignore.include?(file)
+    from = File.join($here, file)
+    to = File.join($home, file)
+
+    if $force || !File.exist?(to)
+      system %{ln -vsfh #{from} #{to}}
     end
   end
 end
+
 puts <<-EOF
 
 See .mybashrc for machine-specific example usage
