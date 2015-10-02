6b6265b79e7fabe6e77143be9aad0ac6fb1b74da
robhurring <robhurring@gmail.com>
2015-10-02 17:53:40 -0400

    + settings

--- shells/zsh/lib/util.zsh
+++ shells/zsh/lib/util.zsh
@@ -1,3 +1,14 @@
 remove_colors() {
   sed -E "s/"$'\E'"\[([0-9]{1,3}((;[0-9]{1,3})*)?)?[m|K]//g"
 }
+
+push_path(){
+  if ! echo $PATH | egrep -q "(^|:)$1($|:)" ; then
+    if [ "$2" = "after" ] ; then
+      PATH=$PATH:$1
+    else
+      PATH=$1:$PATH
+    fi
+  fi
+}
+
