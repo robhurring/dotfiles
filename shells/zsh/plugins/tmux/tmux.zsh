38e533638eb2801400eecf647df36ab04fc79905
robhurring <robhurring@gmail.com>
2015-11-01 18:38:48 -0500

    tmux conf

--- shells/zsh/plugins/tmux/tmux.zsh
+++ shells/zsh/plugins/tmux/tmux.zsh
@@ -1,11 +1,13 @@
 # try to attach by session name, otherwise create a new session
 function tmux-attach-or-new(){
-  tmux attach-session -d -t $1 || tmux new-session -s $1
+  local name=${1:-default}
+  tmux attach-session -d -t $name || tmux new-session -s $name
 }
 
 # try to switch sessions
 function tmux-force-switch(){
-  no-tmux-session && echo "You're not in a tmux session!" || tmux switch -t $1
+  local name=${1:-default}
+  no-tmux-session && echo "You're not in a tmux session!" || tmux switch -t $name
 }
 
 function no-tmux-session(){
