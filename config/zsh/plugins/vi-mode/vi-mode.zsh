8a6ac1ff8fd4bf2ca440315550215593064d9bfe
robhurring <robhurring@gmail.com>
2020-11-24 13:13:00 -0500

    Change cursor shape in VI mode

--- config/zsh/plugins/vi-mode/vi-mode.zsh
+++ config/zsh/plugins/vi-mode/vi-mode.zsh
@@ -6,12 +6,44 @@
 
 bindkey -M viins "$ZSH_VI_ESC" vi-cmd-mode
 
+# Cursor codes: (Default is underscore for command mode)
+#   Ps = 0  ⇒  blinking block.
+#   Ps = 1  ⇒  blinking block (default).
+#   Ps = 2  ⇒  steady block.
+#   Ps = 3  ⇒  blinking underline.
+#   Ps = 4  ⇒  steady underline.
+#   Ps = 5  ⇒  blinking bar, xterm.
+#   Ps = 6  ⇒  steady bar, xterm.
+: ${ZSH_VI_NORMAL_CURSOR:=4}
+: ${ZSH_VI_INSERT_CURSOR:=1}
+
 # Updates editor information when the keymap changes.
 function zle-keymap-select() {
   zstyle ':my:prompt:' vi-mode true
+
+  # https://unix.stackexchange.com/questions/433273/changing-cursor-style-based-on-mode-in-both-zsh-and-vim
+  # escapes:
+  if [[ ${KEYMAP} == vicmd ]] ||
+    [[ $1 = 'block' ]]; then
+      echo -ne "\e[$ZSH_VI_NORMAL_CURSOR q"
+
+    elif [[ ${KEYMAP} == main ]] ||
+      [[ ${KEYMAP} == viins ]] ||
+      [[ ${KEYMAP} = '' ]] ||
+      [[ $1 = 'beam' ]]; then
+          echo -ne "\e[$ZSH_VI_INSERT_CURSOR q"
+  fi
   zle reset-prompt
   zle -R
 }
+
+_fix_cursor() {
+  echo -ne "\e[$ZSH_VI_INSERT_CURSOR q"
+}
+precmd_functions+=(_fix_cursor)
+# zle-line-finish() {
+#   _fix_cursor
+# }
 
 # Ensure that the prompt is redrawn when the terminal size changes.
 TRAPWINCH() {
