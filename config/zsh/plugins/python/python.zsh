b8bbf0e83e611ecb54d5a75e20af3cd20c40a852
Rob Hurring <rhurring@salesforce.com>
2025-09-19 14:58:18 -0400

    fix: remove command check

--- config/zsh/plugins/python/python.zsh
+++ config/zsh/plugins/python/python.zsh
@@ -1,7 +1,3 @@
-if (( ! $+commands[python] )); then
-  return 1
-fi
-
 alias venv='_f(){ local dir=${1:-$([ -d ".venv" ] && echo ".venv" || echo "venv")}; source $dir/bin/activate }; _f'
 alias mkvenv='_f(){ python3 -m venv ${1:-venv} }; _f'
 
