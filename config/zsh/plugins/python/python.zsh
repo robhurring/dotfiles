589c5d6dd017a12736ef269373ddf096fe4ce08b
Rob Hurring <rhurring@salesforce.com>
2025-12-31 09:38:11 -0500

    feat: auto venv

--- config/zsh/plugins/python/python.zsh
+++ config/zsh/plugins/python/python.zsh
@@ -1,3 +1,16 @@
 alias venv='_f(){ local dir=${1:-$([ -d ".venv" ] && echo ".venv" || echo "venv")}; source $dir/bin/activate }; _f'
 alias mkvenv='_f(){ python3 -m venv ${1:-venv} }; _f'
+alias uvs='uv sync --all-extras'
 
+# Auto activate virtualenvs
+auto-venv() {
+  if [[ -d .venv ]]; then
+    source .venv/bin/activate
+  elif [[ -d venv ]]; then
+    source venv/bin/activate
+  elif [[ -n "$VIRTUAL_ENV" ]]; then
+    deactivate
+  fi
+}
+
+add-zsh-hook chpwd auto-venv
