9019d8f529303ff1590a421f2df501446658506f
Rob Hurring <rhurring@salesforce.com>
2026-01-07 14:00:33 -0500

    fix: c-space for filefinder

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -15,6 +15,7 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>r', builtin.oldfiles)
 vim.keymap.set('n', '<c-p>', builtin.find_files, {})
+vim.keymap.set('n', '<c-space>', builtin.find_files, {})
 vim.keymap.set('n', '<m-o>', builtin.find_files, {})
 vim.keymap.set('n', '<leader><leader>', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ps', function()
