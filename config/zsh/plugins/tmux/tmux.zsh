9452461ae9bcdd7809484fa36665d74bdba4ef95
Rob Hurring <rhurring@salesforce.com>
2026-02-16 09:12:53 -0500

    fix: tmux export

--- config/zsh/plugins/tmux/tmux.zsh
+++ config/zsh/plugins/tmux/tmux.zsh
@@ -54,13 +54,18 @@ fi
 
 TMUX_AUTO_REFRESH_ENV="${TMUX_AUTO_REFRESH_ENV:-1}"
 
+TMUX_AUTO_REFRESH_ENV="${TMUX_AUTO_REFRESH_ENV:-1}"
+
 if [ "$TMUX_AUTO_REFRESH_ENV" -eq "1" ]; then
   # Keep the shell updated with tmux's "update-environment" vars when re-attaching
   if [ -n "$TMUX" ]; then
     function refresh-tmux-env {
-    export $(tmux show-environment | grep "^KITTY")
-    export $(tmux show-environment | grep "^SSH")
-  }
+      local vars
+      vars=$(tmux show-environment | grep "^KITTY")
+      [[ -n "$vars" ]] && export ${(f)vars}
+      vars=$(tmux show-environment | grep "^SSH")
+      [[ -n "$vars" ]] && export ${(f)vars}
+    }
   else
     function refresh-tmux-env { }
   fi
