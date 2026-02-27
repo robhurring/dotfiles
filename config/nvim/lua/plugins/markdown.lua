4ed40f691f3f3021bb1aedc97db19ca28f8e1bef
Rob Hurring <rhurring@salesforce.com>
2026-02-27 15:07:30 -0500

    fix: markdown preview scrolling

--- config/nvim/lua/plugins/markdown.lua
+++ config/nvim/lua/plugins/markdown.lua
@@ -3,7 +3,17 @@ return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_preview_options = { disable_sync_scroll = 0 }
+      local function set_sync(val)
+        local opts = vim.g.mkdp_preview_options or {}
+        opts.disable_sync_scroll = val
+        vim.g.mkdp_preview_options = opts
+      end
+      vim.api.nvim_create_autocmd("FocusGained", { callback = function() set_sync(0) end })
+      vim.api.nvim_create_autocmd("FocusLost",   { callback = function() set_sync(1) end })
+    end,
   },
 
   {
