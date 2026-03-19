7a2560ef2e7122d05fc92c46e01bf70c4601aada
robhurring <robhurring@gmail.com>
2026-03-19 12:17:39 -0400

    refactor: toggle sync scroll with keymap

--- config/nvim/lua/plugins/markdown.lua
+++ config/nvim/lua/plugins/markdown.lua
@@ -5,14 +5,13 @@ return {
     ft = { "markdown" },
     build = "cd app && yarn install",
     init = function()
-      vim.g.mkdp_preview_options = { disable_sync_scroll = 0 }
-      local function set_sync(val)
+      vim.g.mkdp_preview_options = { disable_sync_scroll = 1 }
+      vim.keymap.set("n", "<leader>ms", function()
         local opts = vim.g.mkdp_preview_options or {}
-        opts.disable_sync_scroll = val
+        opts.disable_sync_scroll = opts.disable_sync_scroll == 0 and 1 or 0
         vim.g.mkdp_preview_options = opts
-      end
-      vim.api.nvim_create_autocmd("FocusGained", { callback = function() set_sync(0) end })
-      vim.api.nvim_create_autocmd("FocusLost",   { callback = function() set_sync(1) end })
+        vim.notify("Sync scroll: " .. (opts.disable_sync_scroll == 0 and "on" or "off"))
+      end, { desc = "Toggle markdown sync scroll" })
     end,
   },
 
